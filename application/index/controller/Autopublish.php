25a4d42c75b8184e489ff6baf49c69fe33f68024
Administrator <congchao0313@163.com>
2018-03-14 07:22:02 +0800

    0314home

--- application/index/controller/Autopublish.php
+++ application/index/controller/Autopublish.php
@@ -7,11 +7,43 @@
  */
 
 namespace app\index\controller;
+include_once "CurlMethod.php";
 
 
-class Autopublish {
+class Autopublish
+{
+
     /*
-     *
+     * 登录
+     * 传递用户名,密码
      * */
+    public function LoginIn(Request $request)
+    {
+        $curl = new CurlMethod();
+        if ($request->isPost()) {
+
+            $data_login = array(
+                'loginAttempt' => 'true',
+                'lang' => 'en-us',
+                'return' => '',
+                'site' => '',
+                'ad' => '',
+          //    'rememberMe' => 'remember',
+                'email' => 'bp1@biteveryday.com',
+                'password' => 'Ped.,_eo123'
+            );
+            $cookie = dirname(__FILE__) . "/" . 'cookie.txt'; //设置cookie保存的路径
+
+            $url_login = "https://my.backpage.com/classifieds/central/index";
+            $rs_login = $curl->login_post($url_login, $cookie, $data_login);
+            if (!$rs_login) {
+                echo 'login fail';
+            } else {
+                echo 'login succeed';
+            }
+            echo '<br>';
+        }
+    }
+
 
 }
